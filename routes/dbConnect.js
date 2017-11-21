3358298fb07ea3d1719273cd2434319fc5acacae
Josan Iracheta <josan.iracheta@gmail.com>
2017-11-21 13:44:54 -0800

    Create connection Pool

--- routes/dbConnect.js
+++ routes/dbConnect.js
@@ -1,7 +1,7 @@
 const mysql = require("mysql");
 //const express = require('express');
 
-const conn = mysql.createConnection({
+const conn = mysql.createPool({
   host: "localhost",
   user: "root",
   password: "",
