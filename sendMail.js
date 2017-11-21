e7b0ed12546bd132840cbb66af01bd32bded4155
Josan Iracheta <josan.iracheta@gmail.com>
2017-11-21 13:53:39 -0800

    Function to send email

---
+++ sendMail.js
@@ -0,0 +1,25 @@
+const nodemailer = require("nodemailer");
+
+function sendMail(toEmail, subject, body) {
+  const transporter = nodemailer.createTransport({
+    service: "gmail",
+    auth: {
+      user: "repup.help@gmail.com",
+      pass: "Repadmin2017!"
+    }
+  });
+
+  const mailOptions = {
+    from: "Rep Up <repup.help@gmail.com>",
+    to: toEmail,
+    subject: subject,
+    html: body
+  };
+
+  transporter.sendMail(mailOptions, (err, info) => {
+    if (err) console.log(err);
+    console.log(info);
+  });
+}
+
+module.exports = sendMail;
