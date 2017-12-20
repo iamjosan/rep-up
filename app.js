0e64820977c791c38380192d96d2114dd89ad4d0
Josan Iracheta <josan.iracheta@gmail.com>
2017-12-20 11:37:39 -0800

    removed one line in bin/www

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ app.use(logger("dev"));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(reactBuild);
+//app.use(reactBuild);
 app.use("/", reactBuild);
 app.use("/db", ladder);
 app.use("/get-new-reps", getNewReps);
