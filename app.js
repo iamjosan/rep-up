90b6e77bc904811273085a0f08c58353cc977b62
Josan Iracheta <josan.iracheta@gmail.com>
2017-12-20 10:02:30 -0800

    Included error view

--- app.js
+++ app.js
@@ -10,8 +10,6 @@ const getNewUsers = require("./routes/getNewUsers");
 var app = express();
 
 const reactBuild = express.static(path.join(__dirname, "/client/build"));
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger("dev"));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -37,8 +35,7 @@ app.use(function(err, req, res, next) {
 
   // render the error page
   res.status(err.status || 500);
-  console.log("There was an error");
-  //res.render("error");
+  res.render("error");
 });
 
 module.exports = app;
